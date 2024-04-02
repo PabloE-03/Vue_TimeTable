721512ec9813e74faafdcc9af88985b738328684
PabloE-03 <peibol.max@gmail.com>
2024-04-02 20:00:56 +0200

    Cambio en la configuracion CORS

--- Reaktor_TimetableServer/src/main/java/es/iesjandula/reaktor/timetable_server/configurations/CORSConfig.java
+++ Reaktor_TimetableServer/src/main/java/es/iesjandula/reaktor/timetable_server/configurations/CORSConfig.java
@@ -1,5 +1,6 @@
 package es.iesjandula.reaktor.timetable_server.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CORSConfig implements WebMvcConfigurer
 {
+	@Value("${urlCors}")
+	private String urlCors;
+	
 	@Override
 	public void addCorsMappings(CorsRegistry registry)
 	{
-		registry.addMapping("/**").allowedOrigins("http://localhost:5173")
+		registry.addMapping("/**").allowedOrigins(urlCors)
 		.allowedMethods("GET","POST","PUT","DELETE")
 		.allowedHeaders("*");
 	}
